173b9993fb02b6633bd14116a10020d5b8ac62d7
Bogus <jablonski.boguslaw@gmail.com>
2020-07-13 22:25:40 +0200

    controler and Index modification

---
+++ src/main/java/pl/bogus/mysite/controller/IntroductionController.java
@@ -0,0 +1,40 @@
+package pl.bogus.mysite;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.bogus.mysite.data.ContentRepository;
+import pl.bogus.mysite.model.Information;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+@org.springframework.stereotype.Controller
+@RequestMapping
+public class Controller {
+
+    ContentRepository contentRepository;
+@Autowired
+    public Controller(ContentRepository contentRepository) {
+        this.contentRepository = contentRepository;
+    }
+
+   /* @GetMapping("/hello")
+    public String hello(){
+        return "index";
+    }*/
+    @GetMapping("/hello")
+    public String test( @RequestParam(value = "param1", required = false) String param, Model model){
+        Information information = contentRepository
+                .findAll()
+                .stream()
+                .filter(a->a.getTitle().equals(param))
+                .max(Comparator.comparingLong(i->i.getCreated().getTime()))
+                .orElseThrow(NoSuchElementException::new);
+    model.addAttribute("test", information.getContent());
+
+        return "index";
+    }
+}
